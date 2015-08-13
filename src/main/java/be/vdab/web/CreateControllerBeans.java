7415ef2bb61ef8d6e09308415bb0d7762a7edc15
Samuel Engelen <sammyangels@gmail.com>
2015-08-13 15:47:50 +0200

    ResourceHandlers toegevoegd

--- src/main/java/be/vdab/web/CreateControllerBeans.java
+++ src/main/java/be/vdab/web/CreateControllerBeans.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -18,5 +19,13 @@ public class CreateControllerBeans extends WebMvcConfigurerAdapter {
 		resolver.setPrefix("/WEB-INF/JSP/");
 		resolver.setSuffix(".jsp");
 		return resolver;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	registry.addResourceHandler("/images/**")
+	.addResourceLocations("/images/"); 
+	registry.addResourceHandler("/styles/**").addResourceLocations("/styles/");
+	registry.addResourceHandler("/scripts/**").addResourceLocations("/scripts/");
 	}
 }
