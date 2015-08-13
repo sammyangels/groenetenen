ff8660241f901055e13798fdacee3adac1b00c64
Samuel Engelen <sammyangels@gmail.com>
2015-08-13 13:46:13 +0200

    Initializer aangemaakt

---
+++ src/main/java/be/vdab/web/Initializer.java
@@ -0,0 +1,32 @@
+package be.vdab.web;
+
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return null;
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter utf8Filter = new CharacterEncodingFilter();
+		utf8Filter.setEncoding("UTF-8");
+		return new Filter[] { utf8Filter };
+	}
+}
