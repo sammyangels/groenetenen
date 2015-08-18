bbc849baac7738b0d7725365b5a4fc11219ef2bd
Samuel Engelen <sammyangels@gmail.com>
2015-08-18 15:55:36 +0200

    16.1 Uitbreiding Unit Test

--- src/test/java/be/vdab/web/FiliaalControllerTest.java
+++ src/test/java/be/vdab/web/FiliaalControllerTest.java
@@ -2,18 +2,23 @@ package be.vdab.web;
 
 import be.vdab.entities.Filiaal;
 import be.vdab.services.FiliaalService;
+import be.vdab.valueobjects.Adres;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.math.BigDecimal;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertSame;
 
 public class FiliaalControllerTest {
 
+    private Filiaal filiaal;
     private FiliaalController filiaalController;
     private List<Filiaal> filialen;
     private FiliaalService filiaalService;
@@ -24,6 +29,9 @@ public class FiliaalControllerTest {
         filiaalService = Mockito.mock(FiliaalService.class);
         Mockito.when(filiaalService.findAll()).thenReturn(filialen);
         filiaalController = new FiliaalController(filiaalService);
+        filiaal = new Filiaal("naam1", true, BigDecimal.ONE, new Date(),
+                new Adres("straat1", "huisnr1", 1, "gemeente1"));
+        Mockito.when(filiaalService.read(1)).thenReturn(filiaal);
     }
 
     @Test
@@ -33,5 +41,18 @@ public class FiliaalControllerTest {
     @Test
     public void findAllMaaktRequestAttribuutFilialen() {
         assertSame(filialen,filiaalController.findAll().getModelMap().get("filialen"));
+    }
+    @Test
+    public void readActiveertJuisteView() {
+        assertEquals("filialen/filiaal", filiaalController.read(1L).getViewName());
+    }
+    @Test
+    public void readMetBestaandeIDGeeftFiliaalTerug() {
+        assertSame(filiaal,
+                filiaalController.read(1).getModelMap().get("filiaal"));
+    }
+    @Test
+    public void readMetOnbestaandeIDGeeftNullTerug() {
+        assertNull(filiaalController.read(666).getModelMap().get("filiaal"));
     }
 }
